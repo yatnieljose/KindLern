c040d6191518b146b5facf75c76c516e54b46e14
Yatniel Ramos <yatnieljose0208@gmail.com>
2023-04-10 04:20:59 +0200

    commit

---
+++ View/TitleScreen.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+class TitleScreen(ttk.Frame):
+
+    def __init__(self, master):
+        ttk.Frame.__init__(self, master)
+        img = Image.open(r"View\Images\title_img.png")
+        title_img = ImageTk.PhotoImage(img)
+        self.title_lbl = ttk.Label(self, image=title_img)
+        self.title_lbl.img = title_img
+        self.title_lbl.config(image=self.title_lbl.img)
+        self.title_lbl.grid()
+
+        self.title_lbl.bind('<ButtonPress>', self.mouse_press)
+
+    def mouse_press(self, event):
+        """global prev
+        print('alive\n')
+        prev = event"""
+        self.destroy()
